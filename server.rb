13b0b27cc488f5dccc89eac2ed67745d4cb2fd54
faizaanshamsi <faizaan2000@gmail.com>
2014-08-14 10:46:44 -0400

    reading mind data works

--- server.rb
+++ server.rb
@@ -2,11 +2,10 @@ require 'sinatra'
 require 'socket'
 require 'chartkick'
 
-get '/' do
+get '/mind' do
   socket = TCPSocket.new('127.0.0.1', 20000)
-  @data = socket.recv( 100 )
+  @data = eval(socket.recv( 200 ))
   socket.close
-
   erb :index
 end
 
@@ -14,6 +13,3 @@ end
 # been included to explicitly show the configuration options.
 set :views, File.dirname(__FILE__) + '/views'
 set :public_folder, File.dirname(__FILE__) + '/public'
-
-# @data = { :delta=>7023617, :theta=>15294464, :lo_alpha=>15209472, :hi_alpha=>13321984,
-#           :lo_beta=>4527616, :hi_beta=>12073472, :lo_gamma=>862464, :mid_gamma=>13637632 }
